7aa34a3619f99582ac42320463e8fa2dd5360b90
szbrooks2017 <strattonbrooks13@gmail.com>
2021-10-15 15:49:04 +0000

    Adding how to insert to the left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node to the left
+ * @parent: is a pointer to the node to the left child in
+ * @value: is the value of the node to the left child in
+ * Return: returns a pointer or null
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	/* create a new node */
+	binary_tree_t *left_node = NULL;
+
+	/* parent check */
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	/* call function that creates new node */
+	left_node = binary_tree_node(parent, value);
+
+	/* link child to left parent */
+	left_node->left = parent->left;
+
+	/* check if exists */
+	if(left_node->left != NULL)
+	{
+		left_node->left->parent = left_node;
+	}
+
+	parent->left = left_node;
+
+	return (left_node);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -37,10 +37,11 @@ typedef struct binary_tree_s heap_t;
 
 /* print function
 static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+static size_t _height(const binary_tree_t *tree);*/
 void binary_tree_print(const binary_tree_t *);
-static size_t _height(const binary_tree_t *tree); */
 
 /* prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif
