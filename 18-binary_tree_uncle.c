e107ad918e6e2df64ea3533cce5e089903cf1cc8
szbrooks2017 <strattonbrooks13@gmail.com>
2021-10-16 22:58:33 +0000

    how to find the uncle of a node!

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - is the sibling of the parent node
+ * @node: is the node's whom uncle to find
+ * Return: the value of the uncle
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (node->parent->parent->left == node->parent)
+	{
+		return (node->parent->parent->right);
+	}
+	else
+	{
+		return (node->parent->parent->left);
+	}
+}
