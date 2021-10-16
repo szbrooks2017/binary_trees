46e0c69c52139908aee3f0f2a6185d4506b08c9a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-10-16 21:55:57 +0000

    Adding to figure out if BT is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - finds the distance from root to farthest leaf
+ * @tree: pointer to the root node of the tree
+ * Return: the number of generation or 0 if null
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
+}
+
+/**
+ * binary_tree_is_perfect - finds if each parent has two nodes
+ * @tree: is the pointer to the root of the tree
+ * Return: returns a boolean int
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
