d9bac4e283991438ad2b45b6f5236b5e1e9b7bc8
szbrooks2017 <strattonbrooks13@gmail.com>
2021-10-16 19:53:10 +0000

    rearranging variables

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 	}
 
-	size_t depth = binary_tree_depth(tree->parent);
+	size_t depth;
+
+	depth = binary_tree_depth(tree->parent);
 
 	return (depth + 1);
 
