26c9d99a343f9edc55811c795674c1bbd9056eaa
szbrooks2017 <strattonbrooks13@gmail.com>
2021-10-16 19:13:54 +0000

    adding null check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,22 +7,22 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
+		return (0);
+	}
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    if (left_height > right_height)
-    {
-        return (left_height + 1);
-    }
-    else
-    {
-        return (right_height + 1);
-    }
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
 }
